309c6e3f639bf8889647139b08d14b63b35bc1ba
Ben Shaver <benpshaver@gmail.com>
2023-02-08 17:03:29 -0500

    chore(versioning): adding __version__ attribute to top level module which reads from distribution package metadata

--- src/daidepp/__init__.py
+++ src/daidepp/__init__.py
@@ -1,3 +1,7 @@
+from importlib.metadata import version
+
 from daidepp.daide_visitor import daide_visitor
 from daidepp.grammar import create_daide_grammar
 from daidepp.keywords import *
+
+__version__ = version("daidepp")
