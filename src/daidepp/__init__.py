d31a7d5b620eec3220631db723957dfb376f1085
Ben Shaver <benpshaver@gmail.com>
2023-02-08 17:45:44 -0500

    chore(versioning): adding third party importlib-metadata if python version is below 3.8

--- src/daidepp/__init__.py
+++ src/daidepp/__init__.py
@@ -1,7 +1,11 @@
-from importlib.metadata import version
 
 from daidepp.daide_visitor import daide_visitor
 from daidepp.grammar import create_daide_grammar
 from daidepp.keywords import *
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
 __version__ = version("daidepp")
